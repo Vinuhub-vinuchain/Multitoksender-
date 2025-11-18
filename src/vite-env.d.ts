12eefa17f2c96a22bfc7e1fbfdfd4bddd0750c49
VINUHUB <aladoemeka1@gmail.com>
2025-11-17 18:20:41 -0800

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface Window {
+  ethereum?: {
+    isMetaMask?: boolean;
+    request?: (args: any) => Promise<any>;
+    on?: (event: string, callback: (...args: any[]) => void) => void;
+    removeListener?: (event: string, callback: (...args: any[]) => void) => void;
+  };
+}
